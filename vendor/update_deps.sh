c854dc0bc1d863326375e523abf1e3df365ff347
Roman Maslennikov <kafkapro@gmail.com>
2026-06-05 12:10:12 +0300

    Update vendor patches

--- vendor/update_deps.sh
+++ vendor/update_deps.sh
@@ -78,10 +78,10 @@ if [[ "${#vendor_repo_dirs[@]}" -gt 0 ]]; then
     fi
 fi
 
-clone_repo "https://github.com/zcash/orchard.git"               "9d89b504c52dc69064ca431e8311a4cd1c279b44" "$VENDOR_DIR/orchard"
+clone_repo "https://github.com/zcash/orchard.git"               "8de172448be10f3a470f9ac83198dc8a185986ad" "$VENDOR_DIR/orchard"
 clone_repo "https://github.com/ferrilab/radium.git"             "3f27e0d827338aee919213fd071b99819a1b9fff" "$VENDOR_DIR/radium"
 clone_repo "https://github.com/rust-bitcoin/rust-secp256k1.git" "secp256k1-0.29.1"                         "$VENDOR_DIR/rust-secp256k1"
-clone_repo "https://github.com/zcash/sapling-crypto.git"        "6a8282be0959b410a0b622cd5eb84f8c3c134078" "$VENDOR_DIR/sapling-crypto"
+clone_repo "https://github.com/zcash/sapling-crypto.git"        "8186b407b47b595a2ea4f04c73d59fdd83bd401f" "$VENDOR_DIR/sapling-crypto"
 clone_repo "https://github.com/zesterer/spin-rs.git"            "502c9dca17c99762184095c9d64c0aedd1db97ff" "$VENDOR_DIR/spin"
 clone_repo "https://github.com/ZcashFoundation/reddsa.git"      "0.5.1"                                    "$VENDOR_DIR/reddsa"
 
